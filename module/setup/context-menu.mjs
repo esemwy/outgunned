76b92f1ad054ee52d49ac503f85a680a07131753
Stephen Youndt <bulgur_saints.1c@icloud.com>
2026-07-01 14:16:58 -0400

    Fix ContextMenu jQuery deprecation warning (Foundry v13)

--- module/setup/context-menu.mjs
+++ module/setup/context-menu.mjs
@@ -1,15 +1,13 @@
 export class OutgunnedContextMenu extends foundry.applications.ux.ContextMenu.implementation
 {
   constructor(element, selector, menuItems, options = {}) {
-    // @TODO: Deprecated in v15
-    options.jQuery ??= true;
+    if (element?.jquery) element = element[0];
     super(element, selector, menuItems, options);
   }
-  
+
     /** @inheritdoc */
   static create(app, element, selector, items, { hookName = "EntryContext", ...options } = {}) {
-    // @TODO: eprecated in v15
-    options.jQuery ??= true;
+    if (element?.jquery) element = element[0];
     options.fixed ??= true;
     app._callHooks?.(className => `get${className}${hookName}`, items);
     return new this(element, selector, items, options);
